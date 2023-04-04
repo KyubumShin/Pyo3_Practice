c560de4e2086535c702ec5f51dbd6e7be0353714
KyubumShin <kbum0617@naver.com>
2023-04-04 21:28:41 +0900

    Add fn image_to_mat mat_to_image

--- pyo3_numpy/src/lib.rs
+++ pyo3_numpy/src/lib.rs
@@ -12,6 +12,42 @@ mod with_opencv;
 pub use with_opencv::*;
 
 
+unsafe fn image_to_mat(img: PyReadonlyArrayDyn<u8>) -> Mat {
+    let shape = img.shape();
+    let slice = img.as_slice().unwrap();
+    let img_mat = Mat::from_slice(slice).unwrap();
+    let img_mat = img_mat.reshape_nd(shape[2] as i32, &[shape[0] as i32, shape[1] as i32]).unwrap();
+    img_mat
+}
+
+unsafe fn mat_to_image(py:Python, img: Mat) -> Py<PyArray<u8, Ix3>> {
+    let (channels, rows, cols) = (img.channels(), img.rows(), img.cols());
+    let pyarray = PyArray::from_slice(py, img.as_slice::<u8>().unwrap());
+    let pyarray = pyarray.reshape((rows as usize, cols as usize, channels as usize)).unwrap();
+    pyarray.to_owned()
+}
+
+#[pyfunction]
+fn image_processing(img: PyReadonlyArrayDyn<u8>, bboxs: Vec<Vec<i32>>) -> PyResult<Vec<Py<PyArray<u8, Ix3>>>> {
+    let img = unsafe {
+        let img_mat = image_to_mat(img);
+        img_mat
+    };
+    let mut croped_imges = Vec::new();
+
+    Python::with_gil(|py| {
+        for bbox in bboxs {
+            let rect_box: Rect = Rect::new(bbox[0], bbox[1], bbox[2], bbox[3]);
+            let croped_img = Mat::roi(&img, rect_box).unwrap();
+            let croped_img = unsafe {
+                    mat_to_image(py, croped_img.clone()) // not continious So cloned
+            };
+            croped_imges.push(croped_img)
+        }
+    });
+    Ok(croped_imges)
+}
+
 #[pyfunction]
 unsafe fn image_load(path: String) -> PyResult<Py<PyArray<u8, Ix3>>> {
     let mat: Mat = imgcodecs::imread(&path, imgcodecs::IMREAD_COLOR).expect("Failed to Read");
@@ -20,8 +56,8 @@ unsafe fn image_load(path: String) -> PyResult<Py<PyArray<u8, Ix3>>> {
     Python::with_gil(|py| {
         let pyarray = PyArray::from_slice(py, mat.as_slice::<u8>().unwrap()); // mat -> pyarrray 1-D array
         let pyarray = pyarray.cast::<u8>(false).unwrap(); // pyarray type 지정
-        let array = pyarray.reshape((rows as usize, cols as usize, channels as usize))?; // reshape를 통한 이미지 변환
-        Ok(array.to_owned())
+        let pyarray = pyarray.reshape((rows as usize, cols as usize, channels as usize))?; // reshape를 통한 이미지 변환
+        Ok(pyarray.to_owned())
     })
 }
 
